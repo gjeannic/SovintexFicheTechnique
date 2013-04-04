daf7d765983cf02a7d6eb5f3ec4bada0aa29005d
Guillaume JEANNIC <gjeannic@gmail.com>
2013-04-04 12:04:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SovintexFicheTechnique::Application.config.secret_token = '1d351f876a391775e2a303f66334668d8dac4431370c7c4cae91da55afd73d9c033d89f35c8480857363201cc7193b4b79c1eaa7c8065bdbe5e3af43498aad49'
